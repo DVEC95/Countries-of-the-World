a1ab211395d3ad74d30313c82cdbb332abdf2362
David Eastcroft <davideastcroft95@gmail.com>
2020-06-17 15:35:47 +0100

    Adds event bus.

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,14 @@
   <div id="app">
     <h1>Countries of the World</h1>
     <countries-list :countries="countries"></countries-list>
+    <country-detail :country="selectedCountry"></country-detail>
   </div>
 </template>
 
 <script>
+import { eventBus } from './main.js'
 import CountriesList from './components/CountriesList.vue'
+import CountryDetail from './components/CountryDetail.vue'
 
 export default {
   name: 'app',
@@ -20,9 +23,14 @@ export default {
     fetch('https://restcountries.eu/rest/v2/all')
     .then(res => res.json())
     .then(countries => this.countries = countries)
+
+    eventBus.$on('selected-country', (country) => {
+      this.selectedCountry = country;
+    })
   },
   components: {
-    "countries-list": CountriesList
+    "countries-list": CountriesList,
+    "country-detail": CountryDetail
   }
 }
 </script>
